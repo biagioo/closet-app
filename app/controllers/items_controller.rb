47186617103e22a8e954fe840494ba5d0a83ce18
biagioo <mendolia2328@gmail.com>
2020-03-31 12:01:03 -0400

    created sign up routes, and erb files for them as well an item index page

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,3 +1,11 @@
 class ItemsController < ApplicationController 
 
+    get '/items' do 
+        redirect '/login' if !session[:user_id]
+        @items = Item.all 
+        @user = User.find(session[:user_id])
+
+        erb :'items/index'
+    end
+
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,22 @@
 class UsersController < ApplicationController
 
+	get '/signup' do
+		if Helpers.is_logged_in?(session)
+			redirect to '/items'
+		end
+		erb :'/users/new'
+	end
+
+	post '/signup' do
+		@user = User.create(params)
+		if @user.valid?
+			@user.save
+			session["user_id"] = @user.id
+			redirect to '/items'
+		else
+			redirect to '/signup'
+		end
+	end
+
+
 end
